bcdf5ba8c04a9d9750d31dcd42b861426296be5d
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-01-24 22:25:57 +0530

    added middleware

--- routes/auth/admin/index.js
+++ routes/auth/admin/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import { adminLogin, adminLogout, createAdmin, createpage, loginPage } from "../../../controllers/auth/admin/index.js";
 import { authAdminMiddleware, tokenCheckMiddleware } from "../../../middleware/tokenCheckMiddleware.js";
 import { ADMIN_CREATE, ADMIN_LOGIN, ADMIN_LOGIN_POST, ADMIN_LOGOUT } from "../../../constans/endpoints.js";
+import { preventLoggedInAccess } from "../../../middleware/adminAuthMiddleware.js";
 
 const route = express.Router()
 
-route.get(ADMIN_LOGIN,authAdminMiddleware,loginPage)
-route.post(ADMIN_LOGIN_POST,authAdminMiddleware,adminLogin)
+route.get(ADMIN_LOGIN,preventLoggedInAccess,loginPage)
+route.post(ADMIN_LOGIN_POST,preventLoggedInAccess,adminLogin)
 
 route.get(ADMIN_CREATE,createpage)
 route.post(ADMIN_CREATE,createAdmin)
