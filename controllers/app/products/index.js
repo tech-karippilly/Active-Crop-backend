e95d99998b6754077d0c225c752d7225422d8450
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-02-03 19:16:06 +0530

    nav name added

--- controllers/app/products/index.js
+++ controllers/app/products/index.js
@@ -1,15 +1,25 @@
 import { HTTP_SUCCESS } from "../../../constans/httpStatus.js"
 import { USER_PRODUCT_DETAILS_PAGE, USER_PRODUCT_PAGE } from "../../../constans/page.js"
-import { Categoery, Product, Review } from "../../../models/index.js"
+import { Categoery, Product, Review, User } from "../../../models/index.js"
+import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
 
 async function productsPage(req,res) {
     try{
         const {id} = req.params
         const products = await Product.find({catagoery_id:id})
         const catagories = await Categoery.find()
-        res.status(HTTP_SUCCESS).render(USER_PRODUCT_PAGE,{isLogin:false,products,catagories,activeCata:id,})
-    }catch(error){
+        const access_token = req.session.accessToken
+        if (access_token){
+            const jwtDecode = jwt.verify(access_token, process.env.JWT_SECRET_ACCESS_TOKEN)
+            const userId = jwtDecode.userId
+            const currentUser = await User.findById(userId)
+        return res.status(HTTP_SUCCESS).render(USER_PRODUCT_PAGE,{isLogin:true,products,catagories,activeCata:id,currentUser})
+        }
 
+        res.status(HTTP_SUCCESS).render(USER_PRODUCT_PAGE,{isLogin:false,products,catagories,activeCata:id,currentUser:{}})
+    }catch(error){
+        console.log(error.message)
     }
 }
 
@@ -20,6 +30,21 @@ async function productDetailsPage(req,res){
         const catagories = await Categoery.find()
         const reviews = await Review.find({ 'product.productId': id })
         const activeCata = await Categoery.findById(cataid)
+        const access_token = req.session.accessToken
+        if (access_token){
+            const jwtDecode = jwt.verify(access_token, process.env.JWT_SECRET_ACCESS_TOKEN)
+            const userId = jwtDecode.userId
+            const currentUser = await User.findById(userId)
+           return res.status(HTTP_SUCCESS).render(USER_PRODUCT_DETAILS_PAGE,{
+            isLogin:true,
+            products,
+            catagories,
+            activeCata:cataid,
+            activeCataName: activeCata ? activeCata.catagoery_name : "Category",
+            reviews,
+            currentUser
+        })
+        }
 
       return  res.status(HTTP_SUCCESS).render(USER_PRODUCT_DETAILS_PAGE,{
         isLogin:false,
@@ -27,15 +52,15 @@ async function productDetailsPage(req,res){
         catagories,
         activeCata:cataid,
         activeCataName: activeCata ? activeCata.catagoery_name : "Category",
-        reviews})
+        reviews,
+        currentUser:{}
+    })
     }catch(error){
-
+        console.log(error.message)
     }
 }
 
-const renderPage =(res,pageName,status,isLogin,products,catagories,reviews,)=>{
-    res.status(status).render(pageName,{isLogin,products,catagories,reviews})
-}
+
 
 export {
     productsPage,
